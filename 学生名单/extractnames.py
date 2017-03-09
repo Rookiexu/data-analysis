c1aab7cd146970cd1ec28a10d2dc143790dea536
Kanch <kanchisme@gmail.com>
2017-03-09 09:56:51 +0800

    更新500+个班级的学生名单。对提取学生名单的脚本增加错误处理。

--- 学生名单/extractnames.py
+++ 学生名单/extractnames.py
@@ -5,25 +5,36 @@ import os
 
 flist = os.listdir()
 
-print(flist)
+#print(flist)
 
 studentlist = ""   #储存结果
+processsum = 0
 for file in flist:
     if file[file.find('.')+1:] == "csv" or file[file.find('.')+1:] == "CSV":
-        applycsall = open(file, 'r')
+        applycsall = open(file, 'r',encoding="GB18030")
         allreader = csv.reader(applycsall)
         allreaderlist = []
+        x = 0
         for line in allreader:
-            print(line)
+            #['20', '2014082021', '胡文昊  ', '男', '']
+            if x >=2:
+                #跳过前两行标题
+                line[2] = line[2].encode('GB18030','ignore').decode('GB18030')
+                processsum += 1
+            try:
+                print(processsum,line)
+            except Exception as e:
+                print(processsum,e)
             allreaderlist.append(line)
+            x+=1
         applycsall.close()
         del allreaderlist[0]
         del allreaderlist[0]
         for student in allreaderlist:
             studentlist += student[2].replace(' ','') + ":" + file[:file.find('.')] + ","
 
-f = open("slist_csa.txt","w")
-f.write(studentlist)
+f = open("slist_csa.txt","wb")
+f.write(studentlist.encode('GB18030','ignore'))
 f.close()
 
 print("\n----\n处理完毕，共",studentlist.count(','),"名学生。")
