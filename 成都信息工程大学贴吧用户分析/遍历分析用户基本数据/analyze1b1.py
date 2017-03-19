495c402941255795d2e03f9482ccb04f1b4409df
Kanch <kanchisme@gmail.com>
2017-03-19 21:26:24 +0800

    更新数据获取脚本

--- 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/analyze1b1.py
+++ 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/analyze1b1.py
@@ -1,3 +1,16 @@
 import BigDataQueryAPI as QueryAPI
 import termanalyze as TermAnalyze
 
+#该脚本用于从目录下的postdata.csv读取数据并用于分析
+#分析用户的活跃时间段，活跃度，关键字，关系链等
+result = []
+print('open postdata.csv')
+f = open("postdata.csv","r",encoding='utf-8',errors='ignore')
+wr = csv.reader(f)
+print('reading data...')
+for line in wr:
+    pass
+f.close()
+print('done.\ndata retrived:',len(result))
+print('start analyzing.')
+#开始循环分析
--- 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/getPostData.py
+++ 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/getPostData.py
@@ -1,5 +1,28 @@
-import database as DB
+import database as DBS
 import pymysql as SQL
+import csv
+
+#该文件用户从数据库下载所有回帖数据，然后将其存放在目录下名为 postdata.csv的文件夹中
 
 
-pass
+print("conecting to the database...")
+DBCONN = SQL.connect(host=DBS.HOST, port=3306,user=DBS.USER,passwd=DBS.PASSWORD,db=DBS.NAME,charset='UTF8')
+DBCUR = DBCONN.cursor()
+print("downloading data...")
+SEL = "SELECT * FROM `postdata`  WHERE 1"
+DBCUR.execute("SET names 'utf8mb4'")
+DBCUR.execute(SEL)
+DBCONN.commit()
+print("fetch_all...")
+result = DBCUR.fetchall()
+print("done.")
+DBCUR.close()
+DBCONN.close()
+print("close connection.")
+print("data retrived=",len(result))
+print("saving data...")
+f = open("postdata.csv","w",encoding='utf-8',errors='ignore')
+wr = csv.writer(f)
+wr.writerows(result)
+f.close()
+print("application completed!")
