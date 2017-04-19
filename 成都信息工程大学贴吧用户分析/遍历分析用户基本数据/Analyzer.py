56061386f2ba66501b9edf170119717c9190e660
Kanch <kanchisme@gmail.com>
2017-04-19 20:56:20 +0800

    重新编码，为了应对小内存服务器。我们需要改写成分段处理的

---
+++ 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/Analyzer.py
@@ -0,0 +1,20 @@
+
+#[帖子ID    储存贴吧名   回帖作者  回帖内容    回帖日期    回帖目标]
+#   0          1           2        3           4         5
+
+#用于分析本地数据的python脚本
+#获取某一用户的发帖总数，由于采用了分段处理，故，这里每次返回给定dataset中的数量
+def getUserPostSum(username,dataset):
+    pass
+
+def getActiveTimeZone(username,days,dataset):
+    pass
+
+def getReadlationCircle(username,dataset):
+    pass
+
+def getKeymap(username,dataset):
+    pass
+
+def getActivityTimeLine(username,dataset):
+    pass
--- 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/analyze1b1.py
+++ 成都信息工程大学贴吧用户分析/遍历分析用户基本数据/analyze1b1.py
@@ -1,8 +1,7 @@
-import BigDataQueryAPI as QueryAPI
-import termanalyze as TermAnalyze
 import pymysql as SQL
 import database as DBS
 import csv
+import Analyzer
 
 #该脚本用于从目录下的postdata.csv读取数据并用于分析
 #分析用户的活跃时间段，活跃度，关键字，关系链等
@@ -29,7 +28,6 @@ print(len(analyzeduserlist),"users has been analyzed,they will be skipped!")
 DBCUR.close()
 DBCONN.close()
 #打开数据集文件
-result = []
 print('open postdata.csv')
 f = open("postdata.csv","r",encoding='utf-8',errors='ignore')
 wr = csv.reader(f)
@@ -38,6 +36,9 @@ csvw = csv.writer(f)
 #首先统计一次数据集文件中有多少位不同的用户
 differuserlist = []
 i = 0
+print("finding user ",end='\r')
+#differuserlist = [line[2] for line in wr if (len(line) > 3 and (line[2] not in analyzeduserlist) and (line[2] not in differuserlist))]
+print("done ",end='\n\n')
 for line in wr:
     print("finding user ",i,end='\r')
     if len(line) > 3:
@@ -45,61 +46,53 @@ for line in wr:
             continue
         differuserlist.append(line[2])
         i+=1
-print('\n',len(differuserlist)," users found!")
+dlen = len(differuserlist)
+print('\n',dlen," users found!")
 #再根据用户数进行遍历，多次在文件内进行查找用户信息
-for user in differuserlist:
+i = 0
+xresult = []
+for username in differuserlist:
     sum_count = 0
-    sum_count30 = 0
     sum_at90 = []
     sum_ur = []
     sum_kmap = []
+    sum_azone = []
+    print("processing..." , str(i) , "/" , dlen,end='\t')
     #以上几个变量是用于算总量的,下面的代码用于循环读取文件内容并分析（从节约内存的角度考虑，我们需要循环读取）
-    while F.EOF:
-        i = 0
+    f.seek(0) # seek to the begining of the file
+    m = 0
+    dataset = []
+    while True:
+        x = 0
+        m+=1
         for line in wr:
             if len(line) !=0:
-                i+=1
-                result.append(line)
-            if i == READ_BUFFER_SIZE:
+                x+=1
+                dataset.append(line)
+            if x == READ_BUFFER_SIZE:
                 break
-        print('done.\ndata retrived:',len(result))
-        print('setting data...')
-        QueryAPI.setDataset(result)
-        #result中的子项目结构：
-        #[帖子ID    储存贴吧名   回帖作者  回帖内容    回帖日期    回帖目标]
-        #   0          1           2        3           4         5
-        #开始循环分析
-        i = 0
-        sumx = len(result)
-        xresult = []
-        for post in result:
-            i+=1
-            info = "process..." + str(i) + "/" + str(sumx) 
-            print(info,end='\r')
-            username = post[2]
-            print(info,"\tstart process...                 ",end='\r')
-            #首先获得该用户的累计发帖量与30天发帖量
-            print(info,"\tget posts sum and 30 days sum...",end='\r')
-            count,count30 = QueryAPI.getTotalPostsSum(username)
-            #其次分析该用户的活跃时间段
-            print(info,"\tanalyzing active timeline...     ",end='\r')
-            azone = QueryAPI.getActivityTimeZone(username)
-            #在分析其90天的活跃度
-            print(info,"\tanalyzing active timezone...      ",end='\r')
-            at90 = QueryAPI.getActivityTimeLine(username,90)
-            #然后分析用户关系链
-            print(info,"\tanalyzing user relation...        ",end='\r')
-            ur = QueryAPI.getReadlationCircle(username)
-            #然后分析该用户的常用关键字
-            print(info,"\tanalyzing keyword...               ",end='\r')
-            kmap = QueryAPI.getKeymap(username)
-            #基本信息分析完成，储存信息到文件
-
+        if x == 0: #when i = 0 which means eof met.
+            break
+        #对读取到的数据进行分段分析
+        print("reading.... round" , str(m) ,end='\r')
+        #首先获得该用户的累计发帖量与30天发帖量
+        count = Analyzer.getTotalPostsSum(username,dataset)
+        #其次分析该用户的活跃时间段
+        azone = Analyzer.getActivityTimeZone(username,dataset)
+        #在分析其90天的活跃度
+        at90 = Analyzer.getActivityTimeLine(username,90,dataset)
+        #然后分析用户关系链
+        ur = Analyzer.getReadlationCircle(username,dataset)
+        #然后分析该用户的常用关键字
+        kmap = Analyzer.getKeymap(username,dataset)
+        #=======================基本信息分析完成，汇总数据=============================
+        sum_count += count
     #id，累计发帖量，近30天发帖量，活跃度，活跃时间段，用户关系链，常用关键字，保留
-    wstr = [str(username),str(count),str(count30),at90,azone,ur,kmap,"1"]
+    wstr = [str(username),str(sum_count),"0",sum_at90,sum_azone,sum_ur,sum_kmap,"1"]
     xresult.append(wstr)
-    print("\nsaving....")
-    csvw.writerows(xresult)
+    i+=1
+print("\nsaving....")
+csvw.writerows(xresult)
 
 print("\ndone.")
 f.close()
